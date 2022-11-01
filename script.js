19a87bbfa811bf32260e6eb4fa8f7b1cda92e9ba
yanamazo <yana.mazo@gmail.com>
2022-11-01 13:34:04 +0100

    Add files via upload
    add script to build sortly search link

---
+++ script.js
@@ -0,0 +1,5 @@
+function linkBuilder() {
+    let barcodeList = document.getElementById("barcodes").value.replaceAll("\n", "+OR+")
+    let link = "https://app.sortly.com/items?direction=ascending&filter=all&isSummaryView=true&keyword=".concat(barcodeList)
+    document.getElementById("sortly").innerHTML = "<a href='"+link+"' target='_blank' rel='noopener noreferrer'>Перейти в Sortly</a>"
+}
