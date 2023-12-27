484a73d0bf6ea4212ec2b4d76265e1dc942da77b
yanamazo <yana.mazo@gmail.com>
2023-12-27 21:52:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function linkBuilder() {
     let barcodeList = document.getElementById("barcodes").value.replaceAll("\n\n", "\n").replaceAll("\n", "+OR+")
     let link = "https://app.sortly.com/items?direction=ascending&filter=all&isSummaryView=true&keyword=".concat(barcodeList)
-    document.getElementById("sortly").innerHTML = "<a href='"+link+"' target='_blank' rel='noopener noreferrer'>Перейти в Sortly</a>"
+    document.getElementById("sortly").innerHTML = "<a href='"+link+"' target='_blank' rel='noopener noreferrer'>Go to Sortly</a>"
 }
